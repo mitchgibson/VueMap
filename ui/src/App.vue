3898fd3652e0a943a707efe78f93e5b1263eb974
knakmitch <mitch.delachevrotitere@knak.com>
2024-11-09 14:53:28 -0500

    pretty ui

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -7,35 +7,51 @@
     </FloatLabel>
     </div>
     <div class="w-full h-full overflow-y-auto">
-      <DataTable :value="searchResults.nodes" class="w-full">
-        <Column field="componentName" header="Component Name"></Column>
-        <Column field="locations" header="Locations">
-          <template #body="slotProps">
-            <ul>
-              <li v-for="location in slotProps.data.locations" :key="location">{{location}}</li>
-            </ul>
+      <TreeTable :value="treeNodes" tableStyle="min-width: 50rem">
+          <Column field="label" header="Component" expander style="width: 30%">
+            <template #body="slotProps">
+            {{ slotProps.node.label }}
           </template>
-        </Column>
-      </DataTable>
+          </Column>
+      </TreeTable>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {FloatLabel} from 'primevue';
-import {InputText} from 'primevue';
-import {DataTable} from 'primevue';
-import {Column} from 'primevue';
+import {FloatLabel, InputText, TreeTable, Column} from 'primevue';
 import { ref } from 'vue';
+import { TreeNode } from 'primevue/treenode';
 
 const componentName = ref<string>('');
 
 const searchResults = ref<any>({})
 
+const treeNodes = ref<TreeNode[]>([]);
+
 async function onSearch() {
   const results = await fetch(`http://127.0.0.1:8080/nodes?dir=/Users/mitchdelachevrotiere/dev/knak/packages/builder/src&filter=${componentName.value}`);
   const data = await results.json();
   searchResults.value = data;
+
+  treeNodes.value = toTreeNodes(data.nodes);
+
+  console.log(treeNodes.value)
+}
+
+function toTreeNodes(data: {[key: string]: {componentName:string, locations: string[]}}): TreeNode[] {
+  return Object.keys(data).map(key => {
+    return {
+      key: key,
+      label: data[key].componentName,
+      children: data[key].locations.map(location => {
+        return {
+          key: location,
+          label: location
+        }
+      })
+    }
+  });
 }
 
 </script>
